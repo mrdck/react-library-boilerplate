f9c7d493a57e0302cff7ea4193f2dca91267003f
mrdck <mrdck@icloud.com>
2017-10-29 12:00:31 +0100

    example dir remove , add .idea to .gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 class Test extends React.Component<null , null> {
     render(){
-        return( <div>test</div>)
+        return( <div>test test test</div>)
     }
 }
 
